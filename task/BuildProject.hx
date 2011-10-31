82846370aa96905294f6e1e38d9da7c73077fe26
David Peek <david.peek@massiveinteractive.com>
2011-10-31 12:12:30 +1100

    Added mcore dependency.

--- task/BuildProject.hx
+++ task/BuildProject.hx
@@ -8,22 +8,20 @@ class BuildProject extends BuildBase
 	{
 		super();
 		
-		project.id = "mapp";
-		project.name = "MassiveApp";
+		project.id = "mmvc";
+		project.name = "MassiveMVC";
 		project.version = "1.0.0";
 		
-		haxelib.name = "mapp";
+		haxelib.name = "mmvc";
 		haxelib.url = "http://massiveinteractive.com";
 		haxelib.username = "massive";
-		haxelib.description = "MassiveApp is a port of the RobotLegs framework, minus events, plus signals.";
+		haxelib.description = "MassiveMVC is a port of the RobotLegs framework, minus events, plus signals.";
 		haxelib.version.set("1.0.0");
 		haxelib.file.add("src/m", "m");
 
 		haxelib.dependency.add("mcore");
-		haxelib.dependency.add("msignal");
 
 		haxe.library.add("mcore");
-		haxe.library.add("msignal");
 		
 		task("ci").require(["test neko as3 js", "build haxelib.zip"]);
 	}
