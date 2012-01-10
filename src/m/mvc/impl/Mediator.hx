4b98606db592661d9c2153456c6f19b75f5f1990
Dominic De Lorenzo <dom@misprintt.net>
2012-01-10 17:30:39 +1100

    added reference to injector in Mediator
    Already in actor and command

--- src/m/mvc/impl/Mediator.hx
+++ src/m/mvc/impl/Mediator.hx
@@ -10,17 +10,21 @@ package m.mvc.impl;
 import m.mvc.base.MediatorBase;
 import m.mvc.api.IMediatorMap;
 import m.mvc.api.IViewContainer;
-
+import m.inject.IInjector;
 /**
  * Abstract MVCS <code>IMediator</code> implementation
  */
 class Mediator<T> extends MediatorBase<T>
 {
 	@inject
+	public var injector:IInjector;
+	
+	@inject
 	public var contextView:IViewContainer;
 	
 	@inject
 	public var mediatorMap:IMediatorMap;
+	
 	
 	public function new()
 	{
